ed0bc7fac614006d5666f72939e7212c1ed05342
Christian Justin <christian.justin23@gmail.com>
2023-11-10 00:19:53 +0700

    Update colour.py
    Tinggal disatuin

--- colour.py
+++ colour.py
@@ -1,38 +1,45 @@
 import cv2
+import os
 import numpy as np
+import math
 
 def RGBtoHSV (imagename):
     #Membaca dan melakukan pre processing
     img = cv2.imread(imagename)
     height, width = img.shape[:2]
-    R,G,B = cv2.split(img)
+    r_channel,g_channel,b_channel = cv2.split(img)
 
     #Normalisasi
-    R = R / 255
-    G = G / 255
-    B = B / 255
+    r_channel = r_channel / 255
+    g_channel = g_channel / 255
+    b_channel = b_channel / 255
 
     #Penampung nilai HSV
-    h = []
-    s = []
-    v = []
+    hsv = []
+    height, width = len(r_channel)
 
-    
     for y in (height):
         for x in (width):
 
-            #Mencari variabel cmax,cmin,dan 
-            cmax = max(R[x][y],G[x][y],B[x][y])
-            cmin = max(R[x][y],G[x][y],B[x][y])
+            #Mencari variabel cmax,cmin,dan delta
+            cmax = max(r_channel[x][y],g_channel[x][y],b_channel[x][y])
+            cmin = max(r_channel[x][y],g_channel[x][y],b_channel[x][y])
             delta = cmax - cmin  
 
             # Mengisi lit H, list S, list V dengan perhitungan
-            h.append(hue(R[x][y],G[x][y],B[x][y],cmax,delta))
-            s.append(saturation(R[x][y],G[x][y],B[x][y],cmax,delta))
-            v.append(cmax)
+            hsv.append(hue(r_channel[x][y],g_channel[x][y],b_channel[x][y],cmax,delta))
+            hsv.append(saturation(r_channel[x][y],g_channel[x][y],b_channel[x][y],cmax,delta))
+            hsv.append(cmax)
 
-    return h,s,v
+    return hsv
 
+def decompose_block(imagename):
+    img = cv2.imread(imagename)
+    array = np.array(img)
+    new_array = np.array_split(array,3)
+    new2_array = [np.array_split(part, 3, axis=1) for part in new_array]
+    
+    return new2_array
 
 def hue (r,g,b,cmax,delta):
     if(delta == 0):
@@ -58,6 +65,19 @@ def saturation (cmax,delta):
 
     return s
 
-#konsepnta udah dapet
-#sisa histogram
-#sama similarity
+def histogram (vektor):
+    hist = np.histogram(vektor,bins=256,range=(0,255))
+    return hist
+
+def Similarity(histogram1, histogram2):
+    dotproduct = np.dot(histogram1, histogram2)
+    euclidian_norm1 = math.sqrt(sum(x**2 for x in histogram1))
+    euclidian_norm2 = math.sqrt(sum(x**2 for x in histogram2))
+
+    if ((euclidian_norm1 * euclidian_norm2) != 0): 
+        similarity = dotproduct / (euclidian_norm1 * euclidian_norm2)   
+    else:
+        similarity = 0
+
+    return similarity
+
